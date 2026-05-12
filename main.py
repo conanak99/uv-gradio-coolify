935391d65e242f75b0e0dba20320b42ff035910f
conanak99 <huyhoang8a5@gmail.com>
2026-05-12 21:35:40 +0800

    Add hello world Gradio app with configurable PORT

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+import gradio as gr
+
+
+def greet(name):
+    return f"Hello {name}!"
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs="text",
+    outputs="text",
+    title="Hello World",
+)
+
+if __name__ == "__main__":
+    demo.launch(server_port=int(os.environ.get("PORT", 7860)))
